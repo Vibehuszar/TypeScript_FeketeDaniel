c03637134ded4054b3f67f9b1c4d0e9cb990bb86
Dani <hostnameadmin@protonmail.com>
2022-11-10 15:06:27 +0100

    kesz

--- src/index.js
+++ src/index.js
@@ -1,29 +1,72 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const Statue_1 = require("./Statue");
 let lista = [];
-let regexnev = /[aA-zZ]{1,}/;
+let regexnev = /^[aA-zZ]{1,}$/;
 document.addEventListener('DOMContentLoaded', () => {
     var _a;
     (_a = document.getElementById('felvetel')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => {
-        let title = document.getElementById('title').value;
-        let year = document.getElementById('year').value;
-        let price = document.getElementById('price').value;
-        let height = document.getElementById('height').value;
+        let title = document.getElementById('title');
+        let year = document.getElementById('year');
+        let price = document.getElementById('price');
+        let height = document.getElementById('height');
+        let hibaTitle = document.getElementById('hibaTitle');
+        let hibaYear = document.getElementById('hibaYear');
+        let hibaPrice = document.getElementById('hibaPrice');
+        let hibaHeight = document.getElementById('hibaHeight');
         let validTitle = false;
-        if (regexnev.test(title)) {
+        if (regexnev.test(title.value)) {
             validTitle = true;
+            hibaTitle.innerHTML = "";
+        }
+        else {
+            validTitle = false;
+            hibaTitle.style.color = "red";
+            hibaTitle.innerHTML = ("A szobor neve ne legyen üres. Csak angol ABC karaktereket, szőköz ill. vessző és kötőjel karaktereket tartalmazhat csak!");
         }
         let validYear = false;
-        if (parseInt(year) <= 2022) {
+        if (parseInt(year.value) <= 2022) {
             validYear = true;
+            hibaYear.innerHTML = "";
+        }
+        else {
+            validYear = false;
+            hibaYear.style.color = "red";
+            hibaYear.innerHTML = "A készítés éve max. az aktuális évszám lehet!";
         }
         let validPrice = false;
-        if (parseInt(price) >= 1) {
+        if (parseInt(price.value) >= 1) {
             validPrice = true;
+            hibaPrice.innerHTML = "";
+        }
+        else {
+            validPrice = false;
+            hibaPrice.style.color = "red";
+            hibaPrice.innerHTML = "Az érték min. 1 Ft kell, hogy legyen!";
         }
         let validHeight = false;
-        if (parseInt(height) >= 20) {
+        if (parseInt(height.value) >= 20) {
             validHeight = true;
+            hibaHeight.innerHTML = "";
+        }
+        else {
+            validHeight = false;
+            hibaHeight.style.color = "red";
+            hibaHeight.innerHTML = "A magasság min 20 cm legyen!";
+        }
+        if (validTitle && validYear && validPrice && validHeight) {
+            lista.push(new Statue_1.Statue(title.value, parseInt(year.value), parseInt(price.value), parseInt(height.value)));
+            title.value = "";
+            year.value = "";
+            price.value = "";
+            height.value = "";
+            let lbl = document.getElementById('sum');
+            let counter = lista.length;
+            let sum = 0;
+            for (let l of lista) {
+                sum += l.price;
+            }
+            lbl.innerHTML = "Művek darabszáma: " + counter + "db , Összesített ár: " + sum + "Ft";
         }
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,20 @@ import { Statue } from "./Statue";
 
 
 let lista: Artwork[] = [];
-let regexnev: RegExp = /[aA-zZ]{1,}/;
+let regexnev: RegExp = /^[aA-zZ]{1,}$/;
 
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('felvetel')?.addEventListener('click', ()=>{
-        let title = (document.getElementById('title') as HTMLInputElement).value;
-        let year = (document.getElementById('year') as HTMLInputElement).value;
-        let price = (document.getElementById('price') as HTMLInputElement).value;
-        let height = (document.getElementById('height') as HTMLInputElement).value;
+        let title = document.getElementById('title') as HTMLInputElement;
+        let year = document.getElementById('year') as HTMLInputElement;
+        let price = document.getElementById('price') as HTMLInputElement;
+        let height = document.getElementById('height') as HTMLInputElement;
         let hibaTitle = document.getElementById('hibaTitle') as HTMLElement;
         let hibaYear = document.getElementById('hibaYear') as HTMLElement;
         let hibaPrice = document.getElementById('hibaPrice') as HTMLElement;
         let hibaHeight = document.getElementById('hibaHeight') as HTMLElement;
         let validTitle: boolean = false;
-        if(regexnev.test(title)){
+        if(regexnev.test(title.value)){
                 validTitle = true;
                 hibaTitle.innerHTML = "";
         } 
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         let validYear: boolean = false;
-        if(parseInt(year) <= 2022){
+        if(parseInt(year.value) <= 2022){
                 validYear = true;
                 hibaYear.innerHTML = "";
         }else{
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         let validPrice: boolean = false;
-        if(parseInt(price)>= 1){
+        if(parseInt(price.value)>= 1){
                 validPrice = true;
                 hibaPrice.innerHTML = "";
         }else{
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         let validHeight: boolean = false;
-        if(parseInt(height) >= 20){
+        if(parseInt(height.value) >= 20){
                 validHeight = true;
                 hibaHeight.innerHTML = "";
         }else{
@@ -57,8 +57,21 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
 
         if(validTitle && validYear && validPrice && validHeight){
-        ;
+        lista.push(new Statue(title.value,parseInt(year.value),parseInt(price.value),parseInt(height.value)));
+        title.value = "";
+        year.value = "";
+        price.value = "";
+        height.value = "";
+        let lbl = document.getElementById('sum') as HTMLElement;
+        let counter = lista.length;
+        let sum = 0;
+        for (let l of lista) {
+            sum+= l.price;
         }
+        lbl.innerHTML = "Művek darabszáma: " + counter + "db , Összesített ár: " + sum + "Ft"
+        }
+
+
 
 
     })
